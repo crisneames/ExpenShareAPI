f7ade37f691d5cfb26326af416ae91d0c5db91d8
crisneames <crisneames@gmail.com>
2023-11-02 19:36:26 -0500

    finish user repository and controller

--- ExpenShareAPI/Repositories/IUserRepository.cs
+++ ExpenShareAPI/Repositories/IUserRepository.cs
@@ -5,5 +5,11 @@ namespace ExpenShareAPI.Repositories
     public interface IUserRepository
     {
         List<User> GetAllUsers();
+        User GetById(int id);
+        void Add(User user);
+        void Update(User user);
+        void Delete(int id);
+
+
     }
 }
